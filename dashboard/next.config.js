eaf0ef215fd15b391a82b02f4fc79f1948f323d2
sxsevenxperts <markanodigital@gmail.com>
2026-03-21 11:07:26 -0300

    fix: remover next-pwa que causava erros de workbox em desenvolvimento
    - Remover importação do next-pwa que gerava erro de workbox-webpack-plugin
    - Manter configuração Next.js limpa e funcional
    - Evitar dependências desnecessárias de service worker em dev

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,30 +1,17 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/api\./i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'api-cache',
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 5 * 60, // 5 minutes
-        },
-      },
-    },
-  ],
-});
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    // Remover problemas com workbox
+    config.externals.push({
+      'workbox-webpack-plugin': 'workbox-webpack-plugin',
+    });
+    return config;
+  },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
